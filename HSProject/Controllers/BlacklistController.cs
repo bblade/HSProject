7e26582d715dc8c8e14c5df32e5760c63b991595
Mikhail Kutyin <mikhail@kutyin.ru>
2023-12-04 19:50:19 +0300

    alternative filtering #1

--- HSProject/Controllers/BlacklistController.cs
+++ HSProject/Controllers/BlacklistController.cs
@@ -10,20 +10,28 @@ public class BlacklistController : ControllerBase {
         IEnumerable<Goods> goods = blacklistDto.Goods;
         IEnumerable<BlacklistEntry> blacklistEntries = blacklistDto.Blacklist;
 
-        var filteredGoods = goods.Select(good => {
-            var matchedBlacklistIds = blacklistEntries
-                .Where(entry => good.Title.Split(' ')
-                    .Intersect(entry.Text.Split(' '), StringComparer.OrdinalIgnoreCase).Any())
-                .Select(entry => entry.Id)
-                .ToList();
+        var filteredGoods = goods
+            .Where(g =>
+                blacklistEntries.Any(entry =>
+                    g.Title
+                        .Split(' ')
+                        .Intersect(entry.Text.Split(' '), StringComparer.OrdinalIgnoreCase).Any()
+                )
+            )
+            .Select(g => {
+                var matchedBlacklistIds = blacklistEntries
+                    .Where(entry => g.Title.Split(' ')
+                        .Intersect(entry.Text.Split(' '), StringComparer.OrdinalIgnoreCase).Any())
+                    .Select(entry => entry.Id)
+                    .ToList();
 
-            return new {
-                good.Id,
-                good.Title,
-                MatchedBlacklistIds = matchedBlacklistIds.Distinct()
-            };
-        }).Where(good => good.MatchedBlacklistIds.Any()) // Filter out goods with no matched blacklist entries
-        .ToList();
+                return new {
+                    g.Id,
+                    g.Title,
+                    MatchedBlacklistIds = matchedBlacklistIds.Distinct()
+                };
+            })
+            .ToList();
 
         return Ok(filteredGoods);
     }
